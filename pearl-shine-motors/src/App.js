1e5ac14355a7d1d01482c29326d464d8e22b41ca
Eugene <korir16071@gmail.com>
2021-10-25 15:39:24 +0300

    Updated App.js and added components

--- pearl-shine-motors/src/App.js
+++ pearl-shine-motors/src/App.js
@@ -1,10 +1,10 @@
-import Hello from './components/Hello'
 import './App.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-    <Hello/>
+    <Header/>
     
     </div>
   );
---
+++ pearl-shine-motors/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+        <div className="header">
+            <h4>Pearl Shine Motors</h4>
+        </div>            
+        </div>
+    )
+}
+
+export default Header

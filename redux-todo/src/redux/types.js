206c2e7d117cae46bc8639881e661313626069e6
Eugene <korir16071@gmail.com>
2021-11-01 13:48:41 +0300

    Modified todosReducer.js

--- redux-todo/src/redux/types.js
+++
@@ -1,5 +0,0 @@
-export const ADD_COUNT = "ADD_COUNT";
-export const SUBTRACT_COUNT = "SUBTRACT_COUNT";
-export const ADD_TODO = "ADD_TODO";
-export const REMOVE_TODO = "REMOVE_TODO";
-export const MARK_TODO_COMPLETE = "MARK_TODO_COMPLETE";

f7f91808f8d1b930f9f5f5869d8dba5d6518e877
Eugene <korir16071@gmail.com>
2021-11-03 12:13:24 +0300

    Modified AddRemoveFromCart.jsx component

--- pearl-shine-motors/src/components/mini components/addRemoveFromCart.jsx
+++ pearl-shine-motors/src/components/mini components/addRemoveFromCart.jsx
@@ -5,9 +5,7 @@ import { increaseQuantity, reduceQuantity} from '../../redux/actions/cartActions
 const AddRemoveFromCart = ({id})=> {
 
     const {cart} = useSelector(state => state.cart)
-    const itemInCart = cart.find(cartItem=>cartItem.id === id)
-
-    
+    const itemInCart = cart.find(cartItem=>cartItem.id === id)    
     const dispatch = useDispatch()
     const increasingQuantity= (id)=>{
         dispatch(increaseQuantity(id))
@@ -19,9 +17,9 @@ const AddRemoveFromCart = ({id})=> {
     return (
         <div>
             <div>
-                <button onClick={()=>reducingQuantity()}>-</button>
+                <button onClick={()=>reducingQuantity({id})}>-</button>
                 {itemInCart.quantity}
-                <button onClick={()=>increasingQuantity()}>+</button>
+                <button onClick={()=>increasingQuantity({id})}>+</button>
             </div>
             
         </div>

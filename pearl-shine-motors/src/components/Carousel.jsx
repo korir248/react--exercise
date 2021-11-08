fb219474d37d34baccae55a2efa2b91613e6cc65
Eugene <korir16071@gmail.com>
2021-11-08 17:45:14 +0300

    Updated cartReducer.js

---
+++ pearl-shine-motors/src/components/Carousel.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+
+const Carousel = () => { 
+    // const carouselImages = [
+    //     "./../resources/carousel/car1.jpg"
+    // ]
+
+
+    
+    return (
+        <div>
+            <div className="carousel">
+            
+            </div> 
+        </div>
+    )
+}
+
+export default Carousel
